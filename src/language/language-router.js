b748261190ec651fbf15deb45c87e07eeb8d1f2a
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-12 14:25:38 -0500

    took out console logs added a few comments

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const LanguageService = require('./language-service');
-const { requireAuth } = require('../middleware/jwt-auth');
+const express = require("express");
+const LanguageService = require("./language-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const languageRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -8,7 +8,7 @@ const jsonBodyParser = express.json();
 languageRouter.use(requireAuth).use(async (req, res, next) => {
   try {
     const language = await LanguageService.getUsersLanguage(
-      req.app.get('db'),
+      req.app.get("db"),
       req.user.id
     );
 
@@ -24,10 +24,10 @@ languageRouter.use(requireAuth).use(async (req, res, next) => {
   }
 });
 
-languageRouter.get('/', async (req, res, next) => {
+languageRouter.get("/", async (req, res, next) => {
   try {
     const words = await LanguageService.getLanguageWords(
-      req.app.get('db'),
+      req.app.get("db"),
       req.language.id
     );
 
@@ -41,10 +41,10 @@ languageRouter.get('/', async (req, res, next) => {
   }
 });
 
-languageRouter.get('/head', async (req, res, next) => {
+languageRouter.get("/head", async (req, res, next) => {
   try {
     const words = await LanguageService.getWord(
-      req.app.get('db'),
+      req.app.get("db"),
       req.language.id
     );
 
@@ -60,8 +60,7 @@ languageRouter.get('/head', async (req, res, next) => {
   }
 });
 
-languageRouter.route('/guess').post(jsonBodyParser, async (req, res, next) => {
-
+languageRouter.route("/guess").post(jsonBodyParser, async (req, res, next) => {
   try {
     const { guess } = req.body;
 
@@ -69,31 +68,30 @@ languageRouter.route('/guess').post(jsonBodyParser, async (req, res, next) => {
       res.status(400).json({
         error: "Missing 'guess' in request body"
       });
-    }
-    else {
+    } else {
       const data = await LanguageService.guess(
-        req.app.get('db'),
+        req.app.get("db"),
         req.language.id,
         guess
       );
-  
+
       const nextWord = await LanguageService.getWord(
-        req.app.get('db'),
+        req.app.get("db"),
         req.language.id
       );
-  
+
       const language = await LanguageService.getUsersLanguage(
-        req.app.get('db'),
+        req.app.get("db"),
         req.user.id
       );
-  
+
       data.nextWord = nextWord.original;
       data.wordCorrectCount = nextWord.correct_count;
       data.wordIncorrectCount = nextWord.incorrect_count;
       data.totalScore = language.total_score;
       res.status(200).json(data);
     }
-  
+
     next();
   } catch (error) {
     next(error);
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,7 +1,7 @@
 class LinkedList {
   setHead(word, db) {
     if (word.next) {
-      return db('language')
+      return db("language")
         .update({ head: word.next })
         .where({ id: word.language_id });
     }
@@ -16,25 +16,22 @@ class LinkedList {
       if (tempnode.next) {
         prevtempnode = { ...tempnode };
         tempnode = await db
-          .from('word')
-          .select('*')
+          .from("word")
+          .select("*")
           .where({ id: tempnode.next })
           .first();
-      }
-      else {
+      } else {
         prevtempnode = { ...tempnode };
         break;
-      
       }
     }
 
-    
-    return db('word')
+    return db("word")
       .where({ id: word.id })
       .update({ next: prevtempnode.next })
 
       .then(() => {
-        return db('word')
+        return db("word")
           .where({ id: prevtempnode.id })
           .update({ next: word.id });
       });
@@ -44,93 +41,95 @@ class LinkedList {
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
-      .from('language')
+      .from("language")
       .select(
-        'language.id',
-        'language.name',
-        'language.user_id',
-        'language.head',
-        'language.total_score'
+        "language.id",
+        "language.name",
+        "language.user_id",
+        "language.head",
+        "language.total_score"
       )
-      .where('language.user_id', user_id)
+      .where("language.user_id", user_id)
       .first();
   },
 
   getLanguageWords(db, language_id) {
     return db
-      .from('word')
+      .from("word")
       .select(
-        'id',
-        'language_id',
-        'original',
-        'translation',
-        'next',
-        'memory_value',
-        'correct_count',
-        'incorrect_count'
+        "id",
+        "language_id",
+        "original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
       )
       .where({ language_id });
   },
 
   getWord(db, id) {
     let head = db
-      .select('head')
-      .from('language')
+      .select("head")
+      .from("language")
       .where({ id: id })
       .first();
     //return from word where id = languages.head
     return db
-      .from('word')
-      .select('*')
+      .from("word")
+      .select("*")
       .where({ id: head })
       .first();
   },
 
   async guess(db, id, guess) {
     const word = await this.getWord(db, id);
-
+    //If the word is correct move the word back in the list by multiples of two exe 2,4,8
     if (guess === word.translation) {
       const newMem = word.memory_value * 2;
       correct = true;
-      return db('word')
-        .increment('correct_count', 1)
+      return db("word")
+        .increment("correct_count", 1)
         .where({ id: word.id })
         .then(() => {
-          return db('language')
-            .increment('total_score', 1)
+          return db("language")
+            .increment("total_score", 1)
             .where({ id: id });
         })
         .then(() => {
-          return db('word')
+          return db("word")
             .update({ memory_value: newMem })
             .where({ id: word.id });
         })
         .then(() => this.shift(db, id, correct));
-    } else {
+    }
+    //otherwise we reset the mem_value to 1 and move it back one space in the list.
+    else {
       const newMem = 1;
       correct = false;
-      return db('word')
-        .increment('incorrect_count', 1)
+      return db("word")
+        .increment("incorrect_count", 1)
         .where({ id: word.id })
         .then(() => {
-          return db('word')
+          return db("word")
             .update({ memory_value: newMem })
             .where({ id: word.id });
         })
         .then(() => this.shift(db, id, correct));
     }
   },
-
+// this shifts the location of the items within the List adjusting their next pointers to appropriate places.
   async shift(db, id, correct) {
     const list = new LinkedList();
     const word = await this.getWord(db, id);
     return list.insert(word, db).then(() => {
       return db
-        .from('word')
+        .from("word")
         .where({ id: word.id })
-        .select('correct_count', 'incorrect_count', 'translation')
+        .select("correct_count", "incorrect_count", "translation")
         .first()
-        .then((res) => {
+        .then(res => {
           return {
             wordCorrectCount: res.correct_count,
             wordIncorrectCount: res.incorrect_count,
